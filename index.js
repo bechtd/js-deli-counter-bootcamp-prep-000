f554e884603c7e668d6ff5e27ad8ed597be62ab9
David Becht <bechtd@gmail.com>
2018-04-17 19:34:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line, person) {
   var nextNumber = line.length + 1;
-  line.push(person);
-  return line;
+  //line.push(person);
+  return `Welcome, ${person}. You are number ${nextNumber} in line.`;
 }
