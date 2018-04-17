de9fd3d2b4e136a0643c5fb2e49fedff16c7bbd1
David Becht <bechtd@gmail.com>
2018-04-17 19:28:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function takeANumber(katzDeliLine, person) {
-  var nextNumber = katzDeliLine.length + 1;
-  katzDeliLine.push([person]: `Welcom, ${person}. You are number ${nextNumber} in line.`);
-  return katzDeliLine;
+function takeANumber(line, person) {
+  var nextNumber = line.length + 1;
+  line.push([person]: `Welcom, ${person}. You are number ${nextNumber} in line.`);
+  return line;
 }
